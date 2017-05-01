67207780261de75631971e8a933e055d3e03c97e
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-01 14:59:02 +0200

    Redux thunk setup.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import DevTools from './components/DevTools'
 import reducers from './reducers'
 
 export default createStore(
   reducers,
-  compose(applyMiddleware(), DevTools.instrument())
+  compose(
+    applyMiddleware(thunkMiddleware),
+    DevTools.instrument()
+  )
 )
