dd3ebbac83207d603dd0cd2ec93a6739b3f76609
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-01 19:54:55 +0200

    Graceful error handling in SignInForm component.

--- src/reducers/signInForm.js
+++ src/reducers/signInForm.js
@@ -1,13 +1,19 @@
-import { UPDATE_SIGN_IN_FORM } from '../actionTypes'
+import {
+  UPDATE_SIGN_IN_FORM,
+  SET_SIGN_IN_FORM_ERROR_MESSAGE
+} from '../actionTypes'
 
 const initialState = {
-  values: {}
+  values: {},
+  errorMessage: null
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
   case UPDATE_SIGN_IN_FORM:
     return { ...state, values: { ...state.values, ...action.values } }
+  case SET_SIGN_IN_FORM_ERROR_MESSAGE:
+    return { ...state, errorMessage: action.errorMessage }
   default:
     return state
   }
