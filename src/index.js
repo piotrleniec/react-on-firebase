c8c394124d6cd15601c3754db2fbcaac15ed4b19
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-01 14:22:12 +0200

    Firebase setup.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import firebase from 'firebase'
+import firebaseConfig from './firebaseConfig'
 import DevTools from './components/DevTools'
 import store from './store'
 import App from './components/App'
 import './index.css'
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <Provider store={store}>
