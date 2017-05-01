b1543c81e5aedec4f3e113c22faeeb71024d95c8
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-01 14:22:12 +0200

    Firebase setup.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyB_PYP0NOnDVzbb9jp7jPGdPfPNyMhuuoU',
+  authDomain: 'react-on-firebase-c2677.firebaseapp.com',
+  databaseURL: 'https://react-on-firebase-c2677.firebaseio.com',
+  projectId: 'react-on-firebase-c2677',
+  storageBucket: 'react-on-firebase-c2677.appspot.com',
+  messagingSenderId: '372979349666'
+}
