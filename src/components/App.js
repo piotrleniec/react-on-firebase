42f1ea65158e1b63d04de840f31d600295602d85
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-01 20:33:34 +0200

    App component for authenticated and anonymous users.

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,33 @@ import SignUpForm from './SignUpForm'
 import SignOutButton from './SignOutButton'
 import SignInForm from './SignInForm'
 
-const App = props => {
-  if (props.currentUser) return <SignOutButton />
-
-  return (
-    <div>
+const Anonymous = () => (
+  <div className="row">
+    <div className="col-xs-offset-4 col-xs-4">
       <SignUpForm />
       <SignInForm />
     </div>
-  )
-}
+  </div>
+)
+
+const Authenticated = props => (
+  <div>
+    <div className="row">
+      <div className="col-xs-12">
+        Signed as {props.currentUser.email}
+        <div className="pull-right">
+          <SignOutButton />
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const App = props => (
+  props.currentUser ?
+    <Authenticated currentUser={props.currentUser} /> :
+    <Anonymous />
+)
 
 const mapStateToProps = state => ({
   currentUser: state.currentUser
