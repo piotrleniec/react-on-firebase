25b9a75d76863f06f998f019ddf6889b11c0597e
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-03 19:44:22 +0200

    Connected Note component to Redux Form.

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 import { destroyNote } from '../../firebase/database/notes'
 
-const Note = props => (
+let Note = props => (
   <div className="panel panel-primary">
     <div className="panel-heading">
       <h3 className="panel-title">
@@ -16,13 +17,26 @@ const Note = props => (
       </h3>
     </div>
     <div className="panel-body">
-      {props.note.text}
+      <form onSubmit={props.handleSubmit}>
+        <Field name="text" component="textarea" className="form-control" />
+      </form>
     </div>
   </div>
 )
 
-const mapStateToProps = (state, ownProps) => ({
-  note: state.notes.find(note => note.id === ownProps.noteId)
-})
+Note = reduxForm()(Note)
+
+const mapStateToProps = (state, ownProps) => {
+  const note = state.notes.find(note => note.id === ownProps.noteId)
+
+  return {
+    form: `updateNoteForm[${ownProps.noteId}]`,
+    initialValues: {
+      id: note.id,
+      text: note.text
+    },
+    note
+  }
+}
 
 export default connect(mapStateToProps)(Note)
