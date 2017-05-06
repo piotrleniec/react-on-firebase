54dbed168f57676f6d023305ec1a4147fe23bdfb
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-06 17:25:09 +0200

    Profile picture uploading.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,6 +1,23 @@
+import firebase from 'firebase'
 import { SET_CURRENT_USER } from '../actionTypes'
 
 export const setCurrentUser = user => ({
   type: SET_CURRENT_USER,
   user
 })
+
+export const updateProfilePicture = image => (_, getState) => {
+  const userId = getState().currentUser.id
+  const splitName = image.name.split('.')
+  const imageExtension = splitName[splitName.length - 1]
+
+  firebase.storage()
+          .ref('profile-pictures')
+          .child(`${userId}.${imageExtension}`)
+          .put(image)
+          .then(imageSnapshot => {
+            firebase.auth()
+                    .currentUser
+                    .updateProfile({ photoURL: imageSnapshot.downloadURL })
+          })
+}
