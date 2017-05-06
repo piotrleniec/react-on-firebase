5dd55ee70dc75e97311f0debf68634e16f998195
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-06 15:31:41 +0200

    Enable hot reloading only in development environment.

--- src/index.js
+++ src/index.js
@@ -20,16 +20,18 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-module.hot.accept('./components/App', () => {
-  const NextApp = require('./components/App').default
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <div>
-        <DevTools />
-        <NextApp />
-      </div>
-    </Provider>,
-    document.getElementById('root')
-  )
-})
+    ReactDOM.render(
+      <Provider store={store}>
+        <div>
+          <DevTools />
+          <NextApp />
+        </div>
+      </Provider>,
+      document.getElementById('root')
+    )
+  })
+}
